4a4fabaecd27ca3c3b86ea242cff4223d58e2672
bhanu577 <dammalapatibhanu@gmail.com>
2024-09-22 16:46:09 +0530

    add routes sequence order

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 
 const iniate = express();
-iniate.use("/getData",(req,res)=>{
-    res.send("Hi bhanu I am from getData");
+
+iniate.use("/getData/2", (req, res) => {
+  res.send("Hi Bhanu You are in route /getData/2");
 });
 
-iniate.use("/getUser",(req,res)=>{
-    res.send("User : Bhanu Age:25")
+iniate.use("/getData", (req, res) => {
+  res.send("Hi bhanu I am from getData");
 });
 
-iniate.use((req,res)=>{
-    res.send("Hi Bhanu")
-    console.log("Inside Hi Bhanu")
-})
-iniate.listen(7777,()=>{
-    console.log("Server is connected to the port 7777...!")
+iniate.use("/getUser", (req, res) => {
+  res.send("User : Bhanu Age:25");
 });
-
+iniate.use("/", (req, res) => {
+  res.send("Hi Bhanu");
+});
+iniate.listen(7777, () => {
+  console.log("Server is connected to the port 7777...!");
+});
