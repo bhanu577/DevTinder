769b8125abae330d1fd71802f24c4e4b3c3ed57c
bhanu577 <dammalapatibhanu@gmail.com>
2024-09-22 17:47:43 +0530

    routing regex

--- src/app.js
+++ src/app.js
@@ -2,16 +2,21 @@ const express = require("express");
 
 const iniate = express();
 
-iniate.get("/user",(req,res)=>{
-    res.send({firstname:"Bhanu",lastname:"Prakash"});
-})
-iniate.post("/user",(req,res)=>{
-    res.send("Data Saved to DB successfully");
-})
+iniate.get("/user", (req, res) => {
+  res.send({ firstname: "Bhanu", lastname: "Prakash" });
+});
+iniate.post("/user", (req, res) => {
+  res.send("Data Saved to DB successfully");
+});
 
-iniate.delete("/user",(req,res)=>{
-    res.send("Data has been deleted successfully");
-})
+iniate.delete("/user", (req, res) => {
+  res.send("Data has been deleted successfully");
+});
+
+iniate.get("/abc/:userid/:name/:lastName", (req, res) => {
+    console.log(req.params)
+  res.send("API Worked");
+});
 iniate.listen(7777, () => {
   console.log("Server is connected to the port 7777...!");
 });
