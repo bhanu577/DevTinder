b551ac040089e258c495b0c1352f28290a628aab
bhanu577 <dammalapatibhanu@gmail.com>
2024-09-22 21:33:04 +0530

    Mutiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,21 +2,24 @@ const express = require("express");
 
 const iniate = express();
 
-iniate.get("/user", (req, res) => {
-  res.send({ firstname: "Bhanu", lastname: "Prakash" });
-});
-iniate.post("/user", (req, res) => {
-  res.send("Data Saved to DB successfully");
-});
-
-iniate.delete("/user", (req, res) => {
-  res.send("Data has been deleted successfully");
-});
-
-iniate.get("/abc/:userid/:name/:lastName", (req, res) => {
-    console.log(req.params)
-  res.send("API Worked");
-});
+iniate.use(
+  "/getUser",
+  (req, res, next) => {
+    console.log("I am in 1st Route");
+    next();
+  },
+  (req, re, next) => {
+    console.log("I am in 2nd Route");
+    next();
+  },
+  [(req, res, next) => {
+    console.log("I am in 3rd Route");
+    next();
+  },
+  (req,res)=>{
+    res.send("Response 4 !!!");
+  }]
+);
 iniate.listen(7777, () => {
   console.log("Server is connected to the port 7777...!");
 });
